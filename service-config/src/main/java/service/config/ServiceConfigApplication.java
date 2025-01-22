2c193fc7fdd9e5910c4edc45ade45b2b550f7363
thradxjhans10 <142849441+thradxjhans10@users.noreply.github.com>
2025-01-21 22:56:11 -0500

    v3

--- service-config/src/main/java/service/config/ServiceConfigApplication.java
+++ service-config/src/main/java/service/config/ServiceConfigApplication.java
@@ -1,20 +1,30 @@
 package service.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ServiceConfigApplication {
+public class ServiceConfigApplication  implements CommandLineRunner {
 
 
-	@Value("${access-token.private-key}")
+	//@Value("${access-token.private-key}")
+	//private String accessTokenPrivateKeyPath;
+
+	@Value("${access-token.private-key:default/private/key/path}")
 	private String accessTokenPrivateKeyPath;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceConfigApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(accessTokenPrivateKeyPath);
+	}
 }
